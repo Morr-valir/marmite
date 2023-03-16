a3b5b4e6ab6b0fc1aef8f4a368318f0fd7b5e8f3
Matthieu Picard <77137944+Morr-valir@users.noreply.github.com>
2023-03-16 09:16:29 +0400

    first commit

---
+++ src/main/java/com/example/marmite/MarmiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.marmite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarmiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarmiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/marmite/MarmiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.marmite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarmiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
