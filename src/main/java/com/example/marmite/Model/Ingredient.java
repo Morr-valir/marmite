d429fab4e81ad4e1a3c8eaaf38db5b60aff22a13
Matthieu Picard <77137944+Morr-valir@users.noreply.github.com>
2023-03-20 08:28:50 +0400

    create entity plats and ingredient

--- src/main/java/com/example/marmite/Model/Ingredient.java
+++ src/main/java/com/example/marmite/Model/Ingredient.java
@@ -1,5 +1,44 @@
 package com.example.marmite.Model;
 
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+@Entity
 public class Ingredient {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+
+    @ManyToMany(mappedBy = "ingredients")
+    private List<Plat> plats;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public List<Plat> getPlats() {
+        return plats;
+    }
+
+    public void setPlats(List<Plat> plats) {
+        this.plats = plats;
+    }
 }
--- src/main/java/com/example/marmite/Model/Plat.java
+++ src/main/java/com/example/marmite/Model/Plat.java
@@ -1,5 +1,45 @@
 package com.example.marmite.Model;
 
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+@Entity
 public class Plat {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private  Long id;
+    private String nom;
+
+    @ManyToMany
+    private List<Ingredient> listIngredients;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public List<Ingredient> getListIngredients() {
+        return listIngredients;
+    }
+
+    public void setListIngredients(List<Ingredient> listIngredients) {
+        this.listIngredients = listIngredients;
+    }
 }
