cfac681c0f8cb73804a5fc400e05ebaf1dca2b0b
Nicolas HOARAU <h.nico.sully@gmail.com>
2023-03-20 11:53:55 +0400

    Ajout sélection d'un ingrédient par id

--- src/main/java/com/example/marmite/Controller/IngredientController.java
+++ src/main/java/com/example/marmite/Controller/IngredientController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.marmite.Model.Ingredient;
 import com.example.marmite.Repository.IngredientRepository;
@@ -28,5 +29,10 @@ public class IngredientController {
     public ResponseEntity<Ingredient> createIngredient(@RequestBody Ingredient ingredient) {
         ingredientRepository.save(ingredient);
         return ResponseEntity.ok(ingredient);
+    }
+
+    @GetMapping("/ingredients/{id}")
+    public ResponseEntity<Ingredient> getIngredientById(@PathVariable Long id) {
+        return ResponseEntity.ok(ingredientRepository.findById(id).orElse(null));
     }
 }
