7bc0146f13f515b48fddf1ddbad235ed8abc5132
Nicolas HOARAU <h.nico.sully@gmail.com>
2023-03-20 12:02:30 +0400

    Ajout suppression d'ingrédient

--- src/main/java/com/example/marmite/Controller/IngredientController.java
+++ src/main/java/com/example/marmite/Controller/IngredientController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -47,5 +48,16 @@ public class IngredientController {
         }
         ingredientRepository.save(ingredientToUpdate);
         return ResponseEntity.ok(ingredientToUpdate);
+    }
+
+    @DeleteMapping("/ingredients/{id}")
+    public ResponseEntity<Ingredient> deleteIngredient(@PathVariable Long id) {
+        Ingredient ingredientToDelete = ingredientRepository.findById(id).orElse(null);
+        if (ingredientToDelete != null) {
+            ingredientRepository.delete(ingredientToDelete);
+            return ResponseEntity.ok(ingredientToDelete);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
