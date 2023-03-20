31830053793b1f9a61272578633232f92d83c8ee
Matthieu Picard <77137944+Morr-valir@users.noreply.github.com>
2023-03-20 09:32:02 +0400

    fix and implement entity categorie

--- src/main/java/com/example/marmite/Model/Categorie.java
+++ src/main/java/com/example/marmite/Model/Categorie.java
@@ -1,5 +1,32 @@
 package com.example.marmite.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Categorie {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String nom;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
 }
