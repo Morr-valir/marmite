1e0f1bf163aa3bdb86076529f3f73e6eaff782fc
Nicolas HOARAU <h.nico.sully@gmail.com>
2023-03-20 11:49:16 +0400

    Ajout de la création d'ingrédients

--- src/main/java/com/example/marmite/Controller/IngredientController.java
+++ src/main/java/com/example/marmite/Controller/IngredientController.java
@@ -9,14 +9,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.marmite.Model.Ingredient;
 import com.example.marmite.Repository.IngredientRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/api")
 public class IngredientController {
     @Autowired
     private IngredientRepository ingredientRepository;
 
-    @GetMapping("/api/ingredients")
+    @GetMapping("/ingredients")
     public ResponseEntity<List<Ingredient>> getAllIngredients() {
         return ResponseEntity.ok((List<Ingredient>) ingredientRepository.findAll());
+    }
+
+    @PostMapping("/ingredients")
+    public ResponseEntity<Ingredient> createIngredient(@RequestBody Ingredient ingredient) {
+        ingredientRepository.save(ingredient);
+        return ResponseEntity.ok(ingredient);
     }
 }
