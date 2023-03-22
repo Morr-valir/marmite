75054831216a48af3b4486beac3eb68fffe1631e
Nicolas HOARAU <h.nico.sully@gmail.com>
2023-03-22 10:21:24 +0400

    Ajout du hashage du mot de passe lors de l'enregistrement d'un nouvel utilisateur
    Vérification du hash du mot de passe via la fonction de bcrypt lors de la connexion

--- src/main/java/com/example/marmite/Repository/RecetteRepository.java
+++ src/main/java/com/example/marmite/Repository/RecetteRepository.java
@@ -1,9 +1,7 @@
 package com.example.marmite.Repository;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 import com.example.marmite.Model.Recette;
 
-@Repository
 public interface RecetteRepository extends CrudRepository<Recette, Long> {
 }
